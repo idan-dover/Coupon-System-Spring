ab5f4bee007f9a1fa83e0bd20f84535c5e447976
IdanDover <111203178+IdanDover@users.noreply.github.com>
2023-06-11 14:23:57 +0300

    modifying deleteExpiredCoupons method in repo to also be applied in the ORM

--- src/main/java/com/jb/CouponSystemSpring/clr/Init.java
+++ src/main/java/com/jb/CouponSystemSpring/clr/Init.java
@@ -198,8 +198,11 @@ public class Init implements CommandLineRunner {
                 .password("1234")
                 .build();
 
-
-
+        company1.addCoupons(List.of(coupon1));
+        company2.addCoupons(List.of(coupon2));
+        company3.addCoupons(List.of(coupon3));
+        company4.addCoupons(List.of(coupon4));
+        company5.addCoupons(List.of(coupon5));
 
         companyRepo.saveAll(List.of(company1, company2, company3, company4, company5));
         customerRepo.saveAll(List.of(customer1, customer2, customer3, customer4, customer5, customer6, customer7, customer8, customer9, customer10));
