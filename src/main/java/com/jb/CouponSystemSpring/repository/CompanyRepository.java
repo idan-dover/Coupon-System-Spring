ded596683801a1c16234dad10c9dc2b995f23035
IdanDover <111203178+IdanDover@users.noreply.github.com>
2023-06-20 17:46:48 +0300

    removed unused methods

--- src/main/java/com/jb/CouponSystemSpring/repository/CompanyRepository.java
+++ src/main/java/com/jb/CouponSystemSpring/repository/CompanyRepository.java
@@ -1,23 +1,16 @@
 package com.jb.CouponSystemSpring.repository;
 
 import com.jb.CouponSystemSpring.beans.Company;
-import com.jb.CouponSystemSpring.beans.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
-public interface CompanyRepository extends JpaRepository<Company,Integer> {
+public interface CompanyRepository extends JpaRepository<Company, Integer> {
 
     boolean existsByName(String name);
+
     boolean existsByEmail(String email);
-
-
-    Optional<Company> findByEmail(String email);
-
 
     boolean existsByEmailAndPassword(String email, String password);
 
--- src/main/java/com/jb/CouponSystemSpring/repository/CustomerRepository.java
+++ src/main/java/com/jb/CouponSystemSpring/repository/CustomerRepository.java
@@ -5,21 +5,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
     boolean existsByEmail(String email);
 
-    Optional<Customer> findByEmail(String email);
-
     boolean existsByEmailAndPassword(String email, String password);
+
+    boolean existsByIdAndCouponsId(int customerId, int couponId);
 
     @Query(value = "SELECT `id` FROM `coupon-system-spring`.customers WHERE `email`=?", nativeQuery = true)
     int findIdByEmail(String email);
-
-    boolean existsByIdAndCouponsId(int customerId, int couponId);
 
 }
 
