f3b2651d0b4c70d74b4c94832d7a763aaf6155e9
IdanDover <111203178+IdanDover@users.noreply.github.com>
2023-06-25 20:12:54 +0300

    adding "throws" to endpoints in CustomerController

--- src/main/java/com/jb/CouponSystemSpring/controllers/CustomerController.java
+++ src/main/java/com/jb/CouponSystemSpring/controllers/CustomerController.java
@@ -32,12 +32,14 @@ public class CustomerController {
     }
 
     @GetMapping("/coupon/category")
-    public List<Coupon> getAllCoupons(@RequestHeader("token") UUID token, @RequestParam Category val) throws CouponException {
+    public List<Coupon> getAllCoupons(@RequestHeader("token") UUID token, @RequestParam Category val)
+            throws CouponException {
         return customerService.getAllCoupons(token, val);
     }
 
     @GetMapping("/coupon/price")
-    public List<Coupon> getAllCoupons(@RequestHeader("token") UUID token, @RequestParam double val) throws CouponException {
+    public List<Coupon> getAllCoupons(@RequestHeader("token") UUID token, @RequestParam double val)
+            throws CouponException {
         return customerService.getAllCoupons(token, val);
     }
 
